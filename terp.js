2d447dd7e1b53db6777c276fec6469390b349955
Darius Bacon <darius@wry.me>
2014-05-12 13:25:30 -0400

    initial draft

---
+++ terp.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var dbg = 0;
+
+var Tau = 2 * Math.PI;
+
+function makeMachine(program, bot) {
+    var q = [];
+    function toString() {
+        return program.toString() + ' / ' + q.toString();
+    }
+    function receive(message) {
+        q.push(message);
+    }
+    function run(nsteps) {
+        for (; 0 < nsteps; --nsteps) {
+            if (dbg) console.log('q', q);
+            if (!step())
+                return false;
+        }
+        return true;
+    }
+    function step() {
+        for (var i = 0; i < q.length; ++i) {
+            var qi = q[i];
+            switch (qi) {
+            case 'b':
+            case 'f':
+            case 'l':
+            case 'r': {
+                q.splice(i, i+1);
+                bot[qi]();
+                return true;
+            }
+            case '0':
+            case '1': {
+                var d = parseInt(qi);
+                var replacement = program[d].split('');
+                q.splice.apply(q, [i, i+1].concat(replacement)); // wow that was ugly
+                return true;
+            }
+            }
+        }
+        return false;
+    }
+    return {
+        receive: receive,
+        run: run,
+        step: step,
+        toString: toString,
+    };
+}
+
+function makeBot(x, y, heading) {
+    var stepping = 10/250;
+    function step(distance) {
+        ctx.moveTo(x, y);
+        x += distance * Math.cos(heading);
+        y += distance * Math.sin(heading);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+    function forward() {
+        if (dbg) console.log('forward');
+        step(stepping);
+    }
+    function backward() {
+        if (dbg) console.log('backward');
+        step(-stepping);
+    }
+    function left() {
+        if (dbg) console.log('left');
+        heading += Tau/16;
+    }
+    function right() {
+        if (dbg) console.log('right');
+        heading -= Tau/16;
+    }
+    return {
+        f: forward,
+        b: backward,
+        l: left,
+        r: right,
+    };
+}
+
+var m;
+var ctx;
+var aProgram = ['fff1',
+                'rflfrb'];
+
+function start() {
+    ctx = canvas.getContext('2d');
+    ctx.translate(250, 250);
+    ctx.scale(250, -250);
+    ctx.lineWidth = 1/250;
+
+    
+    m = makeMachine(aProgram, makeBot(0, 0, Tau/4));
+    m.receive('0');
+}
